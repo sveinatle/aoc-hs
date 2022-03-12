6f0798abaa28148a631b62188db45ae4d7ea0036
Svein Atle Arnesen <saa@scanreach.com>
2022-03-12 22:24:19 +0100

    Solve day 01 part 2

--- app/Day01.hs
+++ app/Day01.hs
@@ -1,5 +1,6 @@
 module Main where
 
+import Data.List (tails)
 import Data.String (IsString)
 import qualified MyLib (someFunc)
 
@@ -13,13 +14,18 @@ main = do
   testData <- loadData "app/Day01Test.txt"
   problemData <- loadData "app/Day01Problem.txt"
 
-  let testResult = solve testData
-  if testResult == 7
-    then putStrLn $ "Test SUCCEEDED. Problem result = " ++ show (solve problemData)
-    else putStrLn $ "Test FAILED. Result: " ++ show testResult
+  let testResult = solveA2 testData
+   in if testResult == 7
+        then putStrLn $ "Test A SUCCEEDED. Problem result = " ++ show (solveA2 problemData)
+        else putStrLn $ "Test A FAILED. Result: " ++ show testResult
 
-solve :: [String] -> Int
-solve vs = countInc 0 (map read vs)
+  let testResult = solveB testData
+   in if testResult == 5
+        then putStrLn $ "Test B SUCCEEDED. Problem result = " ++ show (solveB problemData)
+        else putStrLn $ "Test B FAILED. Result: " ++ show testResult
+
+solveA :: [String] -> Int
+solveA lines = countInc 0 (map read lines)
 
 countInc :: Int -> [Int] -> Int
 countInc sum [] = sum
@@ -28,4 +34,19 @@ countInc sum (a : b : rest)
   | a < b = countInc (sum + 1) (b : rest)
   | otherwise = countInc sum (b : rest)
 
--- Use foldl or something?
+solveA2 :: [String] -> Int
+solveA2 lines = countIfInc $map read lines
+
+countIfInc :: [Int] -> Int
+countIfInc nums =
+  let pairs = zip nums (tail nums)
+   in length $ filter (uncurry (<)) pairs
+
+solveB :: [String] -> Int
+solveB lines = countIfInc $sumBy3 $map read lines
+
+sumBy3 :: [Int] -> [Int]
+sumBy3 nums =
+  let threes = zip3 nums (tail nums) (tail $tail nums)
+      sum3 (x, y, z) = x + y + z
+   in map sum3 threes
