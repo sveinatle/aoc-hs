76e591f8c88874a4c5293828e0e421e17b4dd483
Svein Atle Arnesen <saa@scanreach.com>
2022-03-17 13:58:57 +0100

    Solve day 10 part 2

--- app/Day10.hs
+++ app/Day10.hs
@@ -1,27 +1,40 @@
 module Day10 where
 
-import Data.List (find)
+import Data.Either (fromLeft)
+import Data.List (elemIndex, find, sort)
+import Data.Maybe (fromJust, isJust, isNothing, mapMaybe)
 import DayProblem
 import Debug.Trace (trace)
 
 problems = (P solveA 26397, P solveB 288957)
 
 solveA :: [String] -> Int
-solveA lines = sum $ map scoreLine lines
+solveA lines = sum $ map (fromLeft 0 . checkLine) lines
 
 solveB :: [String] -> Int
-solveB lines = 8
+solveB lines =
+  let results = map checkLine lines
+      scores = mapMaybe computeScore results
+   in sort scores !! (length scores `div` 2)
 
-scoreLine = scoreLine' []
+checkLine :: [Char] -> Either Int [Char]
+checkLine = checkLine' []
   where
     pairs = [('(', ')'), ('[', ']'), ('{', '}'), ('<', '>')]
     scores = [(')', 3), (']', 57), ('}', 1197), ('>', 25137)]
-    scoreLine' stack [] = 0
-    scoreLine' stack (s : ss) = case find ((s ==) . fst) scores of
+    checkLine' stack [] = Right stack
+    checkLine' stack (s : ss) = case find ((s ==) . fst) scores of
       Just (closer, score) ->
         if (head stack, closer) `elem` pairs
-          then scoreLine' (tail stack) ss
-          else score
-      Nothing -> scoreLine' (s : stack) ss
+          then checkLine' (tail stack) ss
+          else Left score
+      Nothing -> checkLine' (s : stack) ss
+
+computeScore :: Either Int [Char] -> Maybe Int
+computeScore (Left invalid) = Nothing
+computeScore (Right stack) = Just $ foldl (\score sym -> score * 5 + symScore sym) 0 stack
+  where
+    scores = ['(', '[', '{', '<']
+    symScore sym = (+) 1 $ fromJust $ elemIndex sym scores
 
 trace' msg value = trace (msg ++ " " ++ show value) value
