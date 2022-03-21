3a4b1ee896e8b2db7800f7a653ceebfcd8b7309d
Svein Atle Arnesen <saa@scanreach.com>
2022-03-21 18:27:07 +0100

    Day 15: Batch vector updates to improve performance

--- app/Day15.hs
+++ app/Day15.hs
@@ -33,25 +33,32 @@ solve lines =
   let w = length (head lines)
       h = length lines
       endCell = (w -1, h -1)
-      cellCostGrid = Vector.fromList $ concat lines
-      isEndCellScoreFinished grid = isFinished $ snd $ gridGetCell grid w endCell
-      initialPathCostGrid = Vector.fromList (zip [0 ..] $ replicate (w * h) Never)
+      cellCostGrid = FastVector (Vector.fromList $ concat lines) []
+      inputMap = InputMap cellCostGrid w h
+      -- Set up state.
+      initialPathCostGrid = FastVector (Vector.fromList (replicate (w * h) Never)) []
       emptyState = State initialPathCostGrid MinQueue.empty 0
-      initialState = stateSetCell w (0, 0) (Pending 0) emptyState
-      State finalState _ _ = until (isEndCellScoreFinished . stateGrid) (finishNextCell cellCostGrid w) initialState
-   in case snd $ gridGetCell finalState w endCell of
+      initialState = stateSetCell inputMap (0, 0) (Pending 0) emptyState
+      -- Run until end cell is finished.
+      isEndCellScoreFinished grid = isFinished $ gridGetCell grid w endCell
+      State finalState _ _ = until (isEndCellScoreFinished . stateGrid) (finishNextCell inputMap) initialState
+   in case gridGetCell finalState w endCell of
         Finished cost -> cost
         _ -> error "Cell not computed."
 
 type Cost = Int
 
-type CellCostGrid = Vector Cost
+type CellCostGrid = FastVector Cost
 
 type Width = Int
+
+type Height = Int
 
 type CellIdx = Int
 
 type CellCoord = (Int, Int)
+
+data InputMap = InputMap CellCostGrid Width Height
 
 data Cell = Never | Pending Cost | Finished Cost deriving (Show)
 
@@ -63,7 +70,7 @@ isFinished :: Cell -> Bool
 isFinished (Finished _) = True
 isFinished _ = False
 
-type PathCostGrid = Vector (CellIdx, Cell)
+type PathCostGrid = FastVector Cell
 
 type PendingCell = (Cost, CellIdx)
 
@@ -71,47 +78,64 @@ type PendingCells = MinQueue PendingCell
 
 data State = State {stateGrid :: PathCostGrid, statePendingCells :: PendingCells, stateFinishedCount :: Int}
 
+data FastVector a = FastVector (Vector a) [(Int, a)]
+
+fvSet :: Int -> a -> FastVector a -> FastVector a
+fvSet idx value (FastVector vec updates) =
+  let updates' = (idx, value) : updates
+   in if length updates > (round $ sqrt $ fromIntegral $ length vec)
+        then FastVector (vec Vector.// updates') []
+        else FastVector vec updates'
+
+fvGet :: Int -> FastVector a -> a
+fvGet idx (FastVector vec updates) = case find ((== idx) . fst) updates of
+  Nothing -> vec Vector.! idx
+  Just update -> snd update
+
+idxToXY :: Width -> CellIdx -> CellCoord
 idxToXY w idx = (idx `mod` w, idx `div` w)
 
 pendingCellsAddCell :: PendingCell -> PendingCells -> PendingCells
 pendingCellsAddCell = MinQueue.insert
 
-gridGetCell :: Vector a -> Width -> CellCoord -> a
-gridGetCell grid w (x, y) = grid Vector.! (y * w + x)
+gridGetCell :: FastVector a -> Width -> CellCoord -> a
+gridGetCell grid w (x, y) = fvGet (y * w + x) grid
 
-stateSetCell :: Width -> CellCoord -> Cell -> State -> State
-stateSetCell w (x, y) newCellValue (State grid pendingCells finCount) =
+stateSetCell :: InputMap -> CellCoord -> Cell -> State -> State
+stateSetCell (InputMap cellCostGrid w h) (x, y) newCellValue (State grid pendingCells finCount) =
   let cellIdx = y * w + x
       newPendingCells = case newCellValue of
         Pending cost -> pendingCellsAddCell (cost, cellIdx) pendingCells
         _ -> pendingCells
-      newGrid = Vector.concat [Vector.take cellIdx grid, Vector.fromList [(cellIdx, newCellValue)], Vector.drop (cellIdx + 1) grid]
+      newGrid = fvSet cellIdx newCellValue grid
    in State newGrid newPendingCells finCount
 
-updateCell :: CellCostGrid -> Width -> Cost -> CellCoord -> State -> State
-updateCell cellCostGrid w pathCost (x, y) (State grid pendingCells finCount)
-  | x < 0 || y < 0 || x >= w || y >= length grid `div` w = state
+updateCell :: InputMap -> Cost -> CellCoord -> State -> State
+updateCell (InputMap cellCostGrid w h) pathCost (x, y) (State grid pendingCells finCount)
+  | x < 0 || y < 0 || x >= w || y >= h = state
   | otherwise =
     let c = (x, y)
+        inputMap = (InputMap cellCostGrid w h)
         newPathCost = pathCost + gridGetCell cellCostGrid w c
-     in case snd $ gridGetCell grid w c of
-          Never -> stateSetCell w c (Pending newPathCost) state
+     in case gridGetCell grid w c of
+          Never -> stateSetCell inputMap c (Pending newPathCost) state
           Pending oldPathCost ->
             if newPathCost < oldPathCost
-              then stateSetCell w c (Pending newPathCost) state
+              then stateSetCell inputMap c (Pending newPathCost) state
               else state
           Finished _ -> state
   where
     state = State grid pendingCells finCount
 
-finishNextCell :: CellCostGrid -> Width -> State -> State
-finishNextCell cellCostGrid w (State grid pendingCells finCount) =
+finishNextCell :: InputMap -> State -> State
+finishNextCell (InputMap cellCostGrid w h) (State grid pendingCells finCount) =
   let ((pathCost, pendingIdx), newPendingCells) = MinQueue.deleteFindMin pendingCells
       (x, y) = log $ idxToXY w pendingIdx
-      log v = if finCount `mod` 100 == 0 then trace (show finCount ++ show pendingCells) v else v
-   in updateCell cellCostGrid w pathCost (x + 1, y) $
-        updateCell cellCostGrid w pathCost (x - 1, y) $
-          updateCell cellCostGrid w pathCost (x, y + 1) $
-            updateCell cellCostGrid w pathCost (x, y - 1) $
-              stateSetCell w (x, y) (Finished pathCost) $
+      inputMap = InputMap cellCostGrid w h
+      log v = if finCount `mod` 10000 == 0 then trace (show (100 * finCount `div` (w * h)) ++ "%") v else v
+   in updateCell inputMap pathCost (x + 1, y) $
+        updateCell inputMap pathCost (x - 1, y) $
+          updateCell inputMap pathCost (x, y + 1) $
+            updateCell inputMap pathCost (x, y - 1) $
+              stateSetCell inputMap (x, y) (Finished pathCost) $
                 State grid newPendingCells (finCount + 1)
