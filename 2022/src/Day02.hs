97b6a1c753e65ee3dfe20c0ba94acbc875b564ac
Svein Atle Arnesen <sveinatle@gmail.com>
2022-12-02 08:47:25 +0100

    2022-02 Part B

--- 2022/src/Day02.hs
+++ 2022/src/Day02.hs
@@ -7,21 +7,35 @@ import Debug.Trace
 
 log2 v = trace (show v) v
 
-cases = [Case solveA "Test" 15, Problem solveA "Problem", Case solveB "Test" 9999, Problem solveB "Problem"]
+cases = [Case solveA "Test" 15, Problem solveA "Problem", Case solveB "Test" 12, Problem solveB "Problem"]
 
 solveA :: [String] -> Int
-solveA lines = sum $ map (calculateScore . readRound) lines
+solveA lines = sum $ map (calculateScore . readRound selectHandIdentity) lines
+
+-- Normalize to 1-3
+normalize v = ((v - 1) `mod` 3) + 1
 
 calculateScore :: (Int, Int) -> Int
 calculateScore (them, us) = case (3 + us - them) `mod` 3 of
-  0 -> 3 + us
-  1 -> 6 + us
-  2 -> 0 + us
+  0 -> 3 + us -- Tie
+  1 -> 6 + us -- Win
+  2 -> 0 + us -- Lose
   _ -> error "unexpected diff"
 
-readRound :: [Char] -> (Int, Int)
-readRound [them, _, us] = (1 + ord them - ord 'A', 1 + ord us - ord 'X')
-readRound _ = error "unexpected input format"
+readRound :: (Int -> Int -> Int) -> [Char] -> (Int, Int)
+readRound handSelector [themChar, _, usChar] =
+  let them = 1 + ord themChar - ord 'A'
+      us = 1 + ord usChar - ord 'X'
+   in (them, handSelector them us)
+readRound handSelector _ = error "unexpected input format"
+
+selectHandIdentity them us = us
+
+selectHandFromOutcome them outcome = case outcome of
+  1 -> normalize (them - 1) -- Lose
+  2 -> them -- Tie
+  3 -> normalize (them + 1) -- Win
+  _ -> error "unexpected input"
 
 solveB :: [String] -> Int
-solveB lines = 0
+solveB lines = sum $ map (calculateScore . readRound selectHandFromOutcome) lines
