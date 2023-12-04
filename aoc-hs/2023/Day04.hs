c23b67a6ca334f1f5eefd471d8f6edaa814e1f7c
Svein Atle Arnesen <sveinatle@gmail.com>
2023-12-04 23:11:28 +0100

    2023-04 Part B

--- aoc-hs/2023/Day04.hs
+++ aoc-hs/2023/Day04.hs
@@ -1,11 +1,15 @@
+{-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Day04 where
 
 import Data.Char (isDigit, isSpace)
 import Data.List (dropWhileEnd, groupBy, intersect, nub, sortBy)
 import Data.List.Split (splitOn)
+import Data.Map (Map)
+import qualified Data.Map as Map
 import Data.Maybe (catMaybes, mapMaybe)
 import qualified Data.Text as T
 import DayProblem (Case (Case, Problem))
@@ -15,25 +19,50 @@ log2 v = trace (show v) v
 
 cases = [Case solveA "Test" 13, Problem solveA "Problem", Case solveB "Test" 30, Problem solveB "Problem"]
 
-data Card = Card {winning :: [Int], mine :: [Int]}
+data Card = Card {matchCount :: Int, cardCount :: Int}
 
 solveA :: [String] -> Int
-solveA = sum . map (calculateScore . countMatches . readCard . T.pack)
+solveA = sum . map (calculateScore . matchCount . readCard . T.pack)
+  where
+    calculateScore :: Int -> Int
+    calculateScore matchCount = if matchCount == 0 then 0 else 2 ^ (matchCount -1)
 
 readCard :: T.Text -> Card
 readCard line =
-  let [gameIdString, winningString, mineString] = map T.strip $ T.split (`elem` [':', '|']) line
+  let [cardIdString, winningString, mineString] = map T.strip $ T.split (`elem` [':', '|']) line
       winning = readNumbers winningString
       mine = readNumbers mineString
-   in Card {..}
+      matchCount = countMatches winning mine
+   in Card {matchCount, cardCount = 1}
   where
-    readNumbers = map (read . T.unpack) . filter (not . T.null) . T.splitOn " "
+    readNumbers :: T.Text -> [Int]
+    readNumbers = map (read . T.unpack) . T.words
 
-countMatches :: Card -> Int
-countMatches (Card winning mine) = length $ intersect winning mine
+    countMatches :: [Int] -> [Int] -> Int
+    countMatches winning mine = length $ intersect winning mine
 
-calculateScore :: Int -> Int
-calculateScore matchCount = if matchCount == 0 then 0 else 2 ^ (matchCount -1)
+type CardId = Int
+
+type Cards = Map CardId Card
 
 solveB :: [String] -> Int
-solveB lines = 0
+solveB = countCards . processWinnings . readCardsMap
+  where
+    readCardsMap :: [String] -> Cards
+    readCardsMap = Map.fromList . zip [1 ..] . map (readCard . T.pack)
+
+    processWinnings :: Cards -> Cards
+    processWinnings cards = foldl processCardId cards (take (length cards) [1 ..])
+
+    processCardId :: Cards -> CardId -> Cards
+    processCardId cards cardId =
+      let card = cards Map.! cardId
+          gainedCount = cardCount card
+          gainedCardIds = [cardId + 1 .. cardId + matchCount card]
+
+          updateCard :: Cards -> CardId -> Cards
+          updateCard cards cardId = Map.adjust (\Card {cardCount = oldCount, ..} -> Card {cardCount = oldCount + gainedCount, ..}) cardId cards
+       in foldl updateCard cards gainedCardIds
+
+    countCards :: Cards -> Int
+    countCards = sum . Map.map cardCount
