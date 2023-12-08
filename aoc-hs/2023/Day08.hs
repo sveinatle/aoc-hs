05e809dfd26af2ee6c31e37f90693302644989cd
Svein Atle Arnesen <sveinatle@gmail.com>
2023-12-09 00:45:00 +0100

    2023-08 Part B (out of memory)

--- aoc-hs/2023/Day08.hs
+++ aoc-hs/2023/Day08.hs
@@ -6,8 +6,8 @@
 module Day08 where
 
 import Control.Monad
-import Control.Monad.State
-import Data.Char (digitToInt, isAlpha, isDigit, isSpace)
+import Control.Monad.State.Strict
+import Data.Char (digitToInt, isAlphaNum, isDigit, isSpace)
 import Data.Function (on)
 import Data.HashMap.Internal.Array (pair)
 import Data.List (dropWhileEnd, find, group, groupBy, intersect, maximumBy, nub, sort, sortBy, transpose)
@@ -22,7 +22,7 @@ import Debug.Trace (trace)
 
 log2 v = trace (show v) v
 
-cases = [Case solveA "Test1" 2, Case solveA "Test2" 6, Problem solveA "Problem", Case solveB "Test" 999, Problem solveB "Problem"]
+cases = [Case solveA "Test1" 2, Case solveA "Test2" 6, Problem solveA "Problem", Case solveB "Test3" 6, Problem solveB "Problem"]
 
 data Instruction = L | R
 
@@ -34,7 +34,7 @@ type Network = Map NodeId Node
 
 data Document = Document {instructions :: [Instruction], network :: Network}
 
-data NavigationState = NavigationState {location :: NodeId, stepCount :: Int}
+data NavigationStateA = NavigationStateA {location :: NodeId, stepCountA :: Int}
 
 readDocument :: [String] -> Document
 readDocument lines =
@@ -49,26 +49,44 @@ readDocument lines =
 
     readNode :: String -> Node
     readNode line =
-      let [nodeId, left, right] = filter (isAlpha . head) . groupBy ((==) `on` isAlpha) $ line
+      let [nodeId, left, right] = filter (isAlphaNum . head) . groupBy ((==) `on` isAlphaNum) $ line
        in Node {..}
 
 solveA :: [String] -> Int
 solveA lines =
   let Document {..} = readDocument lines
-   in evalState (move network instructions) NavigationState {location = "AAA", stepCount = 0}
+   in evalState (move network instructions) NavigationStateA {location = "AAA", stepCountA = 0}
 
-move :: Network -> [Instruction] -> State NavigationState Int
+move :: Network -> [Instruction] -> State NavigationStateA Int
 move network (i : is) = do
-  NavigationState {..} <- get
+  NavigationStateA {..} <- get
   case location of
-    "ZZZ" -> return stepCount
+    "ZZZ" -> return stepCountA
     _ -> do
       let Node {..} = network Map.! location
       case i of
-        L -> put (NavigationState {location = left, stepCount = stepCount + 1})
-        R -> put (NavigationState {location = right, stepCount = stepCount + 1})
+        L -> put (NavigationStateA {location = left, stepCountA = stepCountA + 1})
+        R -> put (NavigationStateA {location = right, stepCountA = stepCountA + 1})
       move network is
 move network [] = error "Goal not found before running out of instructions."
 
+data NavigationStateB = NavigationStateB {locations :: ![NodeId], stepCountB :: !Int}
+
+-- TODO: Can type classes be used to have just one implementation, but still handling both parts?
 solveB :: [String] -> Int
-solveB l = 0
+solveB lines =
+  let Document {..} = readDocument lines
+      startLocations = filter ((== 'A') . last) . map fst . Map.toList $ network
+   in evalState (moveMultiple network instructions) NavigationStateB {locations = startLocations, stepCountB = 0}
+
+moveMultiple :: Network -> [Instruction] -> State NavigationStateB Int
+moveMultiple network (i : is) = do
+  NavigationStateB {..} <- get
+  if all ((== 'Z') . last) $! locations
+    then return stepCountB
+    else do
+      case i of
+        L -> put (NavigationStateB {locations = map (left . (network Map.!)) locations, stepCountB = stepCountB + 1})
+        R -> put (NavigationStateB {locations = map (right . (network Map.!)) locations, stepCountB = stepCountB + 1})
+      moveMultiple network is
+moveMultiple network [] = error "Goal not found before running out of instructions."
