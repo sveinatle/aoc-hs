80b1bfedfc5575979aafa37acaada1998f3e35f6
Svein Atle Arnesen <sveinatle@gmail.com>
2023-12-08 01:25:30 +0100

    2023-07 Part B

--- aoc-hs/2023/Day07.hs
+++ aoc-hs/2023/Day07.hs
@@ -7,33 +7,36 @@ module Day07 where
 
 import Data.Char (digitToInt, isDigit, isSpace)
 import Data.HashMap.Internal.Array (pair)
-import Data.List (dropWhileEnd, find, group, groupBy, intersect, nub, sort, sortBy, transpose)
+import Data.List (dropWhileEnd, find, group, groupBy, intersect, maximumBy, nub, sort, sortBy, transpose)
 import Data.List.Split (chunksOf, splitEvery, splitOn)
 import Data.Map (Map)
 import qualified Data.Map as Map
 import Data.Maybe (catMaybes, mapMaybe)
+import Data.Ord (comparing)
 import qualified Data.Text as T
 import DayProblem (Case (Case, CaseStr, Problem, ProblemStr))
 import Debug.Trace (trace)
 
 log2 v = trace (show v) v
 
-cases = [Case solveA "Test" 6440, Problem solveA "Problem", Case solveB "Test" 999, Problem solveB "Problem"]
+cases = [Case solveA "Test" 6440, Problem solveA "Problem", Case solveB "Test" 5905, Problem solveB "Problem"]
 
 data Hand = Hand {handValue :: Int, bid :: Int, cards :: [Char]} deriving (Ord, Eq, Show)
 
-solveA :: [String] -> Int
-solveA = calculateTotalScore . readHands
-  where
-    calculateTotalScore :: [Hand] -> Int
-    calculateTotalScore = sum . zipWith (\rank Hand {bid} -> rank * bid) [1 ..] . sort
+data Mode = A | B
 
-readHands :: [[Char]] -> [Hand]
-readHands = map readHand
+solveA :: [String] -> Int
+solveA = calculateTotalScore . readHands A
+
+calculateTotalScore :: [Hand] -> Int
+calculateTotalScore = sum . zipWith (\rank Hand {bid} -> rank * bid) [1 ..] . sort
+
+readHands :: Mode -> [String] -> [Hand]
+readHands mode = map readHand
   where
     readHand line =
       let [cards, bidString] = words line
-          handValue = sum (zipWith (\idx cardValue -> (15 ^ idx) * cardValue) [6, 5 .. 1] (handTypeValue cards : map cardValue cards))
+          handValue = sum (zipWith (\idx cardValue -> (15 ^ idx) * cardValue) [6, 5 .. 1] (handTypeValue (replaceCards cards) : map cardValue cards))
           bid = read bidString
        in Hand {..}
 
@@ -42,9 +45,24 @@ readHands = map readHand
       'A' -> 14
       'K' -> 13
       'Q' -> 12
-      'J' -> 11
+      'J' -> case mode of
+        A -> 11
+        B -> 1
       'T' -> 10
       _ -> digitToInt card
+
+    replaceCards = case mode of
+      A -> id
+      B -> jokers
+
+    jokers :: [Char] -> [Char]
+    jokers "JJJJJ" = "KKKKK"
+    jokers cards =
+      let replacement = head . maximumBy (comparing length) . group . sort . filter (/= 'J') $ cards
+       in replace replacement cards
+      where
+        replace :: Char -> [Char] -> [Char]
+        replace replacement cards = map (\card -> if card == 'J' then replacement else card) cards
 
     handTypeValue :: [Char] -> Int
     handTypeValue cards = case reverse . sort . map length . group . sort $ cards of
@@ -57,4 +75,4 @@ readHands = map readHand
       _ -> 0
 
 solveB :: [String] -> Int
-solveB lines = 0
+solveB = calculateTotalScore . readHands B
